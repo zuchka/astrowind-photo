8d12f897ec05345bf5c81d383f0d6a83c677d66d
zuchka <zuchka@protonmail.ch>
2024-10-22 21:58:42 -0700

    DRY up botero bio

---
+++ src/data/artistBios.ts
@@ -0,0 +1,22 @@
+export const FERNANDO_BOTERO = {
+    artist: "FERNANDO BOTERO",
+    lifespan: "1932 - 2023",
+    nationality: "Colombian",
+    description: `
+      Fernando Botero's signature style of creating sculptures
+      with exaggerated and inflated features, suggesting
+      political criticism and satire, coined a new term in the
+      art world: "Boterismo."
+      <br><br>
+      The Colombian artist, well known
+      for representing women, men, daily life, and historical
+      events with wit and irony, began his career as a painter
+      experimenting with proportion and size with his
+      paintings of round, corpulent humans, before switching
+      to sculpture in the 1970s.
+    `,
+    copyright: '© Fernando Botero',
+  };
+  
+  // Add other artist bios here
+  // export const ANOTHER_ARTIST = { ... };
